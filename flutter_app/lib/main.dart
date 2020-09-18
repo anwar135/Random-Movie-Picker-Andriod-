604174fb432b46e7e1a2bec20a70dc1ce07c1918
anwar135 <60161663+anwar135@users.noreply.github.com>
2020-09-18 19:03:12 -0400

    Add files via upload

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+       home: Scaffold(
+       body: SafeArea(
+         child: Text(
+
+         ),
+       ),
+    );
+    );
